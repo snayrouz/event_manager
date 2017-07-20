0038da5fa5af695c2b06d190269ddb3dc4539dd2
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 11:07:10 -0600

    Fix application terminate

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,7 +7,9 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
-  if zipcode.length < 5
+  if zipcode.nil?
+    zipcode = "00000"
+  elsif zipcode.length < 5
     zipcode = zipcode.rjust 5, "0"
   elsif zipcode.length > 5
     zipcode = zipcode[0..4]
