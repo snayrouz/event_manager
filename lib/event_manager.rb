9b365a2b886b117510eeecd3f14c18962f9af363
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 10:53:54 -0600

    Display name and zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,25 @@
+require 'csv'
 puts "Event Manager Initialized!"
 
-lines = File.read "event_attendees.csv"
-lines.each_with_index do |line, index|
-  next if row_index == 1
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
+
+
+
+
+
+
+
+
+
+# lines = File.read "event_attendees.csv"
+# lines.each_with_index do |line, index|
+#   next if row_index == 1
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
